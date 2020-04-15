8ae7109f229ae1b82d436a7a083ae046d5d41ab0
achrafmt <63545327+achrafmt@users.noreply.github.com>
2020-04-15 22:07:57 +0100

    Update and rename currency1.js to currency2.js

--- currency2.js
+++ currency2.js
@@ -26,6 +26,17 @@ function ready () {
         var button = addToCartButton[i]
         button.addEventListener('click', addToCartClicked)
     }
+    var addToCartButton2 = document.getElementsByClassName('item_add2')
+    for (var i = 0; i < addToCartButton2.length; i++) {
+        var button = addToCartButton2[i]
+        button.addEventListener('click', addToCartClicked2)
+    }
+    var cartButton = document.getElementsByClassName('cart-btn')
+    for (var i = 0; i < cartButton.length; i++) {
+        var button = cartButton[i]
+        button.addEventListener('click', cartButtonClicked)
+    }
+ 
 }
 
 
@@ -64,11 +75,44 @@ function addToCartClicked(event) {
     var imageSrc = shopItem.getElementsByClassName('item_thumb')[0].src
     var quantity = shopItem.getElementsByClassName('item_Quantity')[0].value
     addItemToCart(title, price, imageSrc, quantity)
+    updateCartTotal()
 }
 
+function addToCartClicked2(event) {
+    var button = event.target
+    var postBody = document.getElementsByClassName('post-body')[0]
+    var title = postBody.getElementsByClassName('post-title')[0].innerText
+    var priceElement = postBody.getElementsByClassName('item_price')[0].innerText
+    var reg = /\d+/g
+    var price = priceElement.match(reg)[0]
+    var imageSrc = postBody.getElementsByClassName('item_thumb')[0].src
+    var quantity = postBody.getElementsByClassName('item_Quantity')[0].value
+    addItemToCart(title , price, imageSrc, quantity)
+    updateCartTotal()
+}
+
+function cartButtonClicked(event) {
+var button = event.target
+if (document.getElementById('030321').classList.contains('active')) {
+    document.getElementById('030321').setAttribute("style", "display:none") 
+    document.getElementById('030321').classList.remove('active')
+    return
+}
+document.getElementById('030321').setAttribute("style", "display:block")
+document.getElementById('030321').classList.add('active')
+}
+
+
 function addItemToCart(title, price, imageSrc, quantity) {
-    var cartRow = document.createElement('tbody')
+    var cartRow = document.createElement('tr')
+    cartRow.classList.add('itemRow')
     var cartItems = document.getElementsByClassName('cart-item-container')[0]
+    var cartItemNames = cartItems.getElementsByClassName('item-name')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            return
+        }
+    }
     var cartRowContents = `
          <tr class="itemRow">
             <td class="item-thumb"><img src="${imageSrc}"></td>
@@ -83,6 +127,9 @@ function addItemToCart(title, price, imageSrc, quantity) {
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
     updateCartTotal()
+    cartRow.getElementsByClassName('simpleCart_remove')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('item-increment')[0].addEventListener('click', PlusButtonClicked)
+    cartRow.getElementsByClassName('item-decrement')[0].addEventListener('click', lessButtonClicked)
 
 }
 
@@ -95,8 +142,7 @@ function updateCartTotal() {
         var cartRow = cartRows[i]
         var priceElement = cartRow.getElementsByClassName('item-price')[0]
         var quantityElement = cartRow.getElementsByClassName('item-quantity')[0]
-        var price = parseFloat(priceElement.innerText.replace('$',''))
-           //<-- BACK TO THIS AND CHANGE IT TO DA -->
+        var price = parseFloat(priceElement.innerText)
         var quantity = parseFloat(quantityElement.innerText)
         cartRow.getElementsByClassName('item-total')[0].innerText = (price * quantity) + 'دج'
         total = total + ( price * quantity )
