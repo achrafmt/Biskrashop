4c366d1fbcbdfae4860a57c2886188f9f9e65062
achrafmt <63545327+achrafmt@users.noreply.github.com>
2020-05-11 09:38:47 +0100

    Update imglink_href.js

--- imglink_href.js
+++ imglink_href.js
@@ -1,3 +1,10 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', imgLink())
+} else {
+    imgLink()
+}
+function imgLink(){
+    if (document.getElementsByClassName('item-bottom')) {
 var it = document.getElementsByClassName('item-bottom')
 for (var i = 0; i < it.length; i++){
 var link = it[i].getElementsByTagName('a')[0].href
@@ -7,3 +14,4 @@ a.setAttribute('href', link)
 a.innerHTML = `<img class="item_thumb" src="${im.src}"></img>`
 im.parentNode.replaceChild(a, im)
 }
+}}
